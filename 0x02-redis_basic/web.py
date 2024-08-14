5880cbee24aa78dfb9ee3929cd0a251942412b17
9-max-6 <mutukumaxwel@gmail.com>
2024-08-14 19:17:27 +0300

    Fix: test didn't work. reverting

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,27 +7,32 @@ from typing import Callable
 from functools import wraps
 
 
-redis_store = redis.Redis()
+redis_connection = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def cacher(method: Callable) -> Callable:
+    """a function to implement the caching"""
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def wrapper(url: str) -> str:
+        """ the wrapper that adds functionality and calls the
+        decorated function """
+        count_name = f"count:{url}"
+        redis_connection.incr(count_name)
+        cache_key = method.__qualname__ + ":cache"
+        cached_result = redis_connection.get(cache_key)
+        if cached_result:
+            return cached_result.decode('utf-8')
 
-@data_cacher
+        else:
+            result = method(url)
+            if result:
+                redis_connection.set(count_name, 0)
+                redis_connection.setex(cache_key, 10, result)
+                return result
+    return wrapper
+
+
+@cacher
 def get_page(url: str) -> str:
     """a function to fetch from a url using the requests module
     """
