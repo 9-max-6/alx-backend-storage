05c064e6862114baeee6ec7021b7767de8814ed2
9-max-6 <mutukumaxwel@gmail.com>
2024-08-14 15:20:44 +0300

    Fix: addressed pycodestyle issues

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -19,13 +19,14 @@ def count_calls(method: Callable) -> Callable:
         return method(self, data)
     return wrapper
 
+
 class Cache():
     """A class to initialize a redis cache"""
 
     def __init__(self):
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ store method that takes a data argument
@@ -34,7 +35,12 @@ class Cache():
         rand_key = uuid.uuid4()
         if self._redis.mset({str(rand_key): data}):
             return str(rand_key)
-    def get(self, key: str, fn: Callable = None) -> Union[str, float, int, bytes]:
+
+    def get(
+            self,
+            key: str,
+            fn: Callable = None
+            ) -> Union[str, float, int, bytes]:
         """This callable will be used to convert the data
         back to the desired format.
         """
