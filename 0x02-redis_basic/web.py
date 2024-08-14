fe0d8d71f62f2f27f361111e880fa693116cda8b
9-max-6 <mutukumaxwel@gmail.com>
2024-08-14 18:59:54 +0300

    Fix: changed the arguments from args and kwargs to url

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,10 +13,10 @@ redis_connection = redis.Redis()
 def cacher(method: Callable) -> Callable:
     """a function to implement the caching"""
     @wraps(method)
-    def wrapper(*args, **kwargs) -> str:
+    def wrapper(url: str) -> str:
         """ the wrapper that adds functionality and calls the
         decorated function """
-        count_name = f"count:{args[0]}"
+        count_name = f"count:{url}"
         redis_connection.incr(count_name)
         cache_key = method.__qualname__ + ":cache"
         cached_result = redis_connection.get(cache_key)
@@ -24,10 +24,10 @@ def cacher(method: Callable) -> Callable:
             return cached_result.decode('utf-8')
 
         else:
-            result = method(*args, **kwargs)
+            result = method(url)
             if result:
                 redis_connection.set(count_name, 0)
-                redis_connection.setex(cache_key, 10, result.text)
+                redis_connection.setex(cache_key, 10, result)
                 return result
     return wrapper
 
